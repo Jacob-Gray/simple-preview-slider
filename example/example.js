a430df836bbf04daa06fc57ac1e7bdb4f52c7fdf
Jacob Gray <Jacob-Gray@users.noreply.github.com>
2018-05-12 03:18:51 -0400

    Updated example to use highlight

---
+++ example/example.js
@@ -0,0 +1,26 @@
+var example = document.querySelector('iframe').contentDocument,
+    before = example.querySelector('.image-wrapper'),
+    after = example.querySelector('.filter-preview');
+
+/**
+ * Get url things in da pre
+ */
+
+function handleTriggersOfTheLittleBoldTagThingsInThePre(el, target) {
+
+    el.addEventListener('mouseover', function() {
+        target.classList.add('highlight');
+    });
+
+    el.addEventListener('mouseout', function() {
+        target.classList.remove('highlight');
+    });
+}
+
+var triggers = document.querySelectorAll('b');
+
+handleTriggersOfTheLittleBoldTagThingsInThePre(triggers[0], before);
+handleTriggersOfTheLittleBoldTagThingsInThePre(triggers[1], after);
+
+
+
