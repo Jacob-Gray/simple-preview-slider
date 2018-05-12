6f52f446c7ad7ad851e8edd66de48b1cb0e18f1f
Jacob Gray <Jacob-Gray@users.noreply.github.com>
2018-05-11 21:16:46 -0400

    stuff

--- src/filter-preview.js
+++ src/filter-preview.js
@@ -1,1 +1,90 @@
-!function(){var e,t=document.querySelector(".filter-preview"),n=t.querySelector(".image-wrapper"),r=n.querySelector(".filter-slider"),i=!1;function o(e){return parseInt(getComputedStyle(e).width)}function d(){n.style.backgroundSize=o(t)+"px"}window.addEventListener("resize",d),d(),t.addEventListener("mousemove",function(r){if(i){var d=o(t),u=o(n)-(e-r.pageX);u>50&&d-u>50&&(n.style.width=u+"px",e=r.pageX)}}),r.addEventListener("mousedown",function(t){i=!0,e=t.pageX}),window.addEventListener("mouseup",function(){i=!1})}();
+(function () {
+
+	/**
+	 * Select elements
+	 */
+    var $el = document.querySelector('.filter-preview'),
+        $wrapper = $el.querySelector('.image-wrapper'),
+        $slider = $wrapper.querySelector('.filter-slider');
+
+	/**
+	 * Initialize global vars
+	 */
+    var dragging = false,
+        currentWidth,
+        startX;
+
+    function w(el) {
+        return parseInt(getComputedStyle(el).width);
+    }
+
+	/**
+	 * Make sure the two element backgrounds overlap perfectly
+	 */
+    function positionWrapperBackground() {
+        $wrapper.style.backgroundSize = w($el) + 'px';
+    }
+
+    window.addEventListener('resize', positionWrapperBackground);
+    positionWrapperBackground();
+
+	/**
+	 * Handle resize
+	 */
+    window.addEventListener('mousemove', function (event) {
+
+        if (dragging) {
+
+            var displayWidth = w($el);
+
+            currentWidth = w($wrapper) - (startX - event.pageX);
+
+            if (currentWidth <= 3) {
+
+                $slider.classList.add('grow-right');
+            } else if (displayWidth - currentWidth <= 3) {
+
+                $slider.classList.add('grow-left');
+            } else {
+
+                $slider.className = 'filter-slider'
+
+                $wrapper.style.width = (currentWidth / displayWidth) * 100 + '%';
+                startX = event.pageX;
+            }
+        }
+    });
+
+    $slider.addEventListener('mousedown', function (event) {
+
+        dragging = true;
+        startX = event.pageX;
+    });
+
+    window.addEventListener('mouseup', function () {
+
+        dragging = false;
+    });
+
+    window.addEventListener('mouseout', function () {
+
+        if(dragging) {
+
+            var displayWidth = w($el);
+
+            if(currentWidth < 40) {
+
+                $slider.classList.add('grow-right');
+                $wrapper.style.width = '3px';
+            }
+
+            if(displayWidth - currentWidth <= 40) {
+
+                $slider.classList.add('grow-left');
+                $wrapper.style.width = displayWidth - 3 + 'px';
+            }
+        }
+
+        console.log('mouse left');
+    });
+})();
